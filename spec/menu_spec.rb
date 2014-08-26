0b7f57d2461ed8c09f1b7002a22cc6953984e51c
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-26 22:28:42 +0100

    menu tests and model added

---
+++ spec/menu_spec.rb
@@ -0,0 +1,34 @@
+require 'menu'
+
+describe Menu do
+
+	let(:sample_price) 	{ 12.0																											}
+	let(:menu) 					{ Menu.new 											 														}
+	let(:dish) 					{ double :dish, :name => "Fried Chicken", :price => sample_price 	}
+
+	it 'should not contain any dishes initially' do
+		expect(menu.dishes).to eq []
+	end
+
+	it 'should be able to add new dishes' do
+		menu.add_dish(dish)
+		expect(menu.dishes).to eq [dish]
+	end	
+
+	it 'should know if it does not contain a dish' do
+		expect(menu.contains?(dish.name)).to be false
+	end
+
+	it 'should know if it contains a dish' do
+		menu.add_dish(dish)
+		expect(menu.contains?(dish.name)).to be true
+	end
+
+	it 'should print menu to terminal' do
+		menu.add_dish(dish)
+		allow(STDOUT).to receive(:puts)
+		expect(STDOUT).to receive(:puts).with("Fried Chicken: 12.0")
+		menu.display
+	end
+
+end
