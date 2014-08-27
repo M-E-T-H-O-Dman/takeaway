80bb2bb9ff393126d350687915ee1b3bd490cbce
Egalitea-leaves <charlie.redwood@gmail.com>
2014-08-27 13:07:19 +0100

    menu test passing

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,3 +1,28 @@
 class Menu
+	attr_reader :dishes
+
+	def initialize
+		@dishes = []
+	end
+
+	def add_dish(dish)
+		@dishes << dish
+	end
+
+	def contains?(dish)
+		dish_names.include?(dish)
+	end
+
+	def dish_names
+		@dishes.map { |dish| dish.name }
+	end
+
+	def display
+		puts "-------------MENU-------------"
+		@dishes.each do |dish|
+			puts "#{dish.name}: #{dish.price}"
+		end
+		puts "------------------------------"
+	end
 
 end	
